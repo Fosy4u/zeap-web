ed9cddebeeb95db0229959495f1021952cdb2db9
foster <foster.ogwudu@yahoo.com>
2025-05-02 03:05:13 +0100

    fix size guide table

--- src/app/account/reviews/page.tsx
+++ src/app/account/reviews/page.tsx
@@ -6,10 +6,22 @@ import React, { useContext, useEffect } from "react";
 import AccountNavBar from "@/components/account/AccountNavBar";
 
 import MyRecommendedProducts from "@/components/products/MyRecommendedProducts";
+import { useSelector } from "react-redux";
+import { globalSelectors } from "@/redux/services/global.slice";
+import zeapApiSlice from "@/redux/services/zeapApi.slice";
+import Loading from "@/app/loading";
 
 const ReviewsPage = () => {
   const router = useRouter();
+  const token = useSelector(globalSelectors.selectAuthToken);
   const { user } = useContext(AuthContext);
+  const getUserReviewsQuery = zeapApiSlice.useGetUserReviewsQuery(
+    {},
+    { skip: !token }
+  );
+  const reviews = getUserReviewsQuery?.data?.data;
+  const isLoading = getUserReviewsQuery.isLoading;
+  const isFulfilled = getUserReviewsQuery?.status === "fulfilled";
 
   useEffect(() => {
     if (user?.isGuest) {
@@ -24,10 +36,18 @@ const ReviewsPage = () => {
         </div>
         {user && !user?.isGuest && (
           <div className="col-span-1 md:col-span-3 ">
-           <h1 className="text-2xl  font-bold sm:text-3xl lg:text-4xl mb-4">
+            <h1 className="text-2xl  font-bold sm:text-3xl lg:text-4xl mb-4">
               My Reviews
             </h1>
-           
+            {reviews?.length === 0 && isFulfilled && (
+              <div className="flex flex-col items-center justify-center h-full">
+                <h2 className="text-xl font-semibold">No Reviews Yet</h2>
+                <p className="text-gray-500">
+                  Start shopping and share your thoughts!
+                </p>
+              </div>
+            )}
+            {isLoading && <Loading />}
           </div>
         )}
       </div>
