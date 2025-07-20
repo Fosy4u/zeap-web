bed96bf944aca1ddf25461829d12c90c119a015c
foster <foster.ogwudu@yahoo.com>
2025-07-20 03:19:34 +0100

    fix hover bug

--- src/components/Header/MobileNavBar.tsx
+++ src/components/Header/MobileNavBar.tsx
@@ -1,4 +1,7 @@
-import SubMenu from "./SubMenu";
+"use client";
+import Link from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
+import { subNavPrimaryData } from "@/data/content";
 import MobileSubProductPrimaryNav from "./MobileSubProductPrimaryNav";
 import { useEffect, useState } from "react";
 import MobileChildrenLayout from "./childrenSubMenus/MobileChildrenLayout";
@@ -10,13 +13,49 @@ const MobileNavBar = ({
   isVisable: boolean;
   setIsVisable: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const productGroupPage = searchParams.get("productGroupPage") || "HOME";
   const [hovered, setHovered] = useState<string | undefined>(undefined);
+  const getFilteredSubNavData = () => {
+    if (!productGroupPage) {
+      return subNavPrimaryData.filter(
+        (item) => item.productGroupNav === "HOME"
+      );
+    }
+    const navData =
+      subNavPrimaryData.filter(
+        (item) =>
+          item.productGroupNav === productGroupPage ||
+          item.matchedHref === pathname
+      ) || [];
+    //ensure no duplicate labels
+    const uniqueLabels = new Set();
+    return navData.filter((item) => {
+      if (uniqueLabels.has(item.label)) {
+        return false;
+      }
+      uniqueLabels.add(item.label);
+      return true;
+    });
+  };
+
+  const filteredSubNavData = getFilteredSubNavData();
 
   useEffect(() => {
     if (isVisable) {
       setHovered(undefined);
     }
   }, [isVisable]);
+  const getSalesLink = () => {
+    if (productGroupPage === "READY TO WEAR") {
+      return "/sales?productGroupPage=READY TO WEAR";
+    } else if (productGroupPage === "BESPOKE") {
+      return "/sales?productGroupPage=BESPOKE";
+    } else {
+      return "/sales";
+    }
+  };
   return (
     <div>
       <div
@@ -52,7 +91,37 @@ const MobileNavBar = ({
           isVisable ? "opacity-0" : "opacity-100"
         }`}
       >
-        <SubMenu setIsVisable={setIsVisable} />
+        <div className="flex gap-4 w-[100vw] overflow-auto lg:bg-primary lg:justify-start lg:items-center lg:gap-2 lg:px-4 lg:py-2 no-scrollbar">
+          {filteredSubNavData?.length > 0 &&
+            filteredSubNavData.map((item) => (
+              <Link
+                key={item.label + item.productGroupNav}
+                href={{
+                  pathname: item.matchedHref,
+                  query: {
+                    productGroupPage: item.productGroupNav,
+                    subProductGroupPage: item.label,
+                    collectionTitle: item.collectionTitle,
+                  },
+                }}
+                onClick={() => {
+                  if (setIsVisable) setIsVisable(false);
+                }}
+                className={`text-xs font-extrabold text-slate-900 lg:text-white  transition-all duration-300 ease-in-out p-1 lg:px-2`}
+              >
+                {item.label}
+              </Link>
+            ))}
+          <Link
+            href={getSalesLink()}
+            onClick={() => {
+              if (setIsVisable) setIsVisable(false);
+            }}
+            className={`text-xs font-bold text-gold transition-all duration-300 ease-in-out p-1 px-2 cursor-pointer `}
+          >
+            SALES
+          </Link>
+        </div>
       </div>
     </div>
   );
