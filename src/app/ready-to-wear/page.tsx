5d85c5ce7b0c5086d5a614b25272b958f2a4bce8
foster <foster.ogwudu@yahoo.com>
2025-07-27 22:40:22 +0100

    implemented rtw page

--- src/app/ready-to-wear/page.tsx
+++ src/app/ready-to-wear/page.tsx
@@ -1,8 +1,44 @@
+import Link from "next/link";
+import ReadyToWearCategories from "./ReadyToWearCategories";
+
+const backGroundVideoUrl =
+  "https://v.ftcdn.net/08/55/17/10/700_F_855171022_WdV2T0PBBIHtOEIHTT8wwsl7MFkGsEm5_ST.mp4";
 const ReadyToWearPage = () => {
   return (
-    <div className="h-screen flex items-center justify-center font-bold lg:text-2xl">
-      This page is under construction. Please check back later.
-    </div>
+    <>
+      <Link
+        href="/collections/isBespoke=true?productGroupPage=BESPOKE&collectionTitle=Bespoke Collections"
+        className="relative flex h-screen  w-full items-center justify-center bg-black"
+        // style={{
+        //   backgroundImage: `url(${backGroundVideoUrl})`,
+        //   backgroundSize: "cover",
+        //   backgroundPosition: "center",
+        // }}
+      >
+        <video
+          src={backGroundVideoUrl}
+          autoPlay
+          muted
+          loop
+          playsInline
+          preload="none"
+          className="absolute inset-0 w-full h-full object-cover opacity-50"
+        />
+
+        <div className="relative z-10 text-white text-center">
+          <h1 className="text-4xl md:text-6xl font-extrabold mb-4">
+            Ready to Wear Fashion
+          </h1>
+          <p className="text-lg md:text-xl font-bold">
+            Instant Chic. Zero Fuss
+          </p>
+          <button className="mt-6 bg-primary text-white px-6 py-3 rounded-md hover:bg-secondary transition-colors">
+            Explore Our Latest Collections
+          </button>
+        </div>
+      </Link>
+      <ReadyToWearCategories />
+    </>
   );
 };
 
